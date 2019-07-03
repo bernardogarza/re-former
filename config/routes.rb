16aff1a352616ca3eb6804dbf4da1120d4a69091
Bernardo Garza <bernardo.g.landa@gmail.com>
2019-07-03 17:45:43 -0500

    ran rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'users/new' => 'users#new'
   get 'users/create' => 'users#create'
   # get 'users/:id' => 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: %i[new create edit update]
 end
